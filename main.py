06af0c9572156e5869e68a22620e1615733e87be
mikhailBrann <alunos13@yandex.ru>
2021-09-24 18:51:40 +0300

    change cocking func

--- main.py
+++ main.py
@@ -4,23 +4,32 @@ file_name = 'recipes.txt'
 os_separator = os.path.sep
 path_to_file = 'files' + os_separator + file_name
 
-def reading_file(read_file):
-    with open(read_file, 'r', encoding="utf-8") as document:
+def get_shop_list_by_dishes(dishes, person_count):
+    with open(path_to_file, 'r', encoding="utf-8") as document:
         result = {}
         for doc_line in document:
             dish_name = doc_line.strip()
-            amount_ingredients = int(document.readline())
-            ingredients_list = []
+            if dish_name in dishes:
+                
+                amount_ingredients = int(document.readline())
+                ingredients_list = []
 
-            for ingredient in range(amount_ingredients):
-                ingredient_name, quantity, measure = document.readline().split('|')
-                ingredients_list.append({'ingredient_name': ingredient_name.strip(), 'quantity': int(quantity.strip()), 'measure': measure.strip()})
+                for ingredient in range(amount_ingredients):
+                    ingredient_name, quantity, measure = document.readline().split('|')
+                    find_ingredient = ingredient_name.strip()
 
-            result[dish_name] = ingredients_list
-            document.readline()
+                    if find_ingredient in result:
+                        result[find_ingredient]['quantity'] += int(quantity.strip())
+                    else:
+                        result[find_ingredient] = {'measure': measure.strip(), 'quantity': int(quantity.strip())}
+
+                document.readline()
+            
+        for key in result.keys():
+            result[key]['quantity'] *= int(person_count)
 
         return result
 
 
-recipes_book = reading_file(path_to_file)
+recipes_book = get_shop_list_by_dishes(['Фахитос', 'Омлет'], 3)
 pprint.pprint(recipes_book)
